1ea6fe7aa22c0e3ee1c5528cd5ced002ad5ea1f5
NavneetSamdariya <25612566+NavneetSamdariya@users.noreply.github.com>
2021-12-20 23:05:48 +0530

    Update log.sh

--- log.sh
+++ log.sh
@@ -4,7 +4,15 @@ sysbench --test=cpu run
 sysbench --test=memory run
 sysbench --test=fileio --file-test-mode=seqwr run
 
+snap install geekbench4 --edge
 ./geekbench4
+sudo pacman -S git fakeroot patch
+git clone https://aur.archlinux.org/phoronix-test-suite
+cd phoronix-test-suite/
+makepkg -s
+ls *pkg.tar.*
+phoronix-test-suite-10.2.2-1-any.pkg.tar.zst
+sudo pacman -U --noconfirm phoronix-test-suite-10.2.2-1-any.pkg.tar.zst
 pacman -S hardinfo
 phoronix-test-suite install john-the-ripper
 phoronix-test-suite run john-the-ripper
