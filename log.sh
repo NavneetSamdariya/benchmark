747995832a6da95b1d88cd584398a0952d4dead2
NavneetSamdariya <25612566+NavneetSamdariya@users.noreply.github.com>
2021-12-20 22:59:30 +0530

    Create log.sh

---
+++ log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+command |& tee -a output.txt
+sysbench --test=cpu run
+sysbench --test=memory run
+sysbench --test=fileio --file-test-mode=seqwr run
+
+./geekbench4
+pacman -S hardinfo
+phoronix-test-suite install john-the-ripper
+phoronix-test-suite run john-the-ripper
+phoronx-test-suite install luxmark
+phoronix-test-suite run luxmark
+phoronix-test-suite install compile-firefox
+phoronix-test-suite run compile-firefox
+echo The Benchmark is Complete
